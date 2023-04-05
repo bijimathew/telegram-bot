65a8e1d28620758c3d1f3e28ee98c4261222fe5b
bijimathew <bijimatthew@yahoo.com>
2023-04-05 12:23:12 +0530

    changes: message,functions

--- psbot.py
+++ psbot.py
@@ -1,19 +1,17 @@
 import telebot
 import telegram
 
-# set the Telegram Bot API token provided by BotFather
+# token taken by botfather from telegram    
 TOKEN = '6147019965:AAEnRpWt6SrO5dLGQWy6oK-1txQkIX7yqAc'
-
-# create a Telebot object
 bot = telebot.TeleBot(TOKEN)
 
-# define a function for the /start command
+# function for the /start command
 @bot.message_handler(commands=['start'])
 def start(message):
     start_text = 'hello! Welcome to private server bot chat \nType /help for more info'
     bot.reply_to(message,start_text)
 
-# define a function for the /help command
+# function for the /help command
 @bot.message_handler(commands=['help'])
 def help(message):
     help_text = "Available commands:\n\n" \
@@ -25,7 +23,7 @@ def help(message):
 def about(message):
     about_text= "this group is just for information purpose only \n stay safe stay healthy Make sure you subsribe psgenshin_ youtube channel"
     bot.reply_to(message,about_text)
-# define a function for the /files command
+#  /files command
 # define a function for the /file command
 # @bot.message_handler(commands=['files'])
 # def files(message):
@@ -44,41 +42,39 @@ def files(message):
     bot.reply_to(message,files_text)    
 @bot.message_handler(commands=['grasstool'])
 def send_file(message):
-    # open the file you want to send
+    # sending file up
     file = open('D:/coding/project_files/GrasscutterTools-v1.8.0.exe', 'rb')
-    # send the file to the user
+    # send 
     bot.send_document(message.chat.id, file)
-    # close the file
     file.close()
 
 @bot.message_handler(commands=['charbuild'])
 def send_file(message):
-    # open the file you want to send
+    # sending file up
     file = open('D:/coding/project_files/CharacterBuilder.jar','rb')
-    # send the file to the user
+    # send
     bot.send_document(message.chat.id, file)
-    # close the file
     file.close()
 
 @bot.message_handler(commands=['allcode'])
 def send_file(message):
-    # open the file you want to send
+    # sending file up
     file = open('D:/coding/project_files/handbook.txt','rb')
-    # send the file to the user
+    # send
     bot.send_document(message.chat.id, file)
-    # close the file
     file.close()
 
-# define a function for the /about command
+# function for the /about command
 @bot.message_handler(commands=['about'])
 def about(message):
     about_text = "This bot was created by [your name here]. It is powered by Python and the telebot library."
     bot.reply_to(message, about_text)
 
-# define a function for handling text messages
+# normal text message
 @bot.message_handler(func=lambda message: True)
 def echo(message):
-    bot.reply_to(message, message.text)
+    default_text = "please select from the following command\n/start\n/help\n/about"
+    bot.reply_to(message,default_text)
 
-# start the bot
+# starting the bot 
 bot.polling()
